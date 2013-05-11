4e1dea962738ed80c11b877eae5886a400e153fa
Yu Jiang Tham <yoojbruin@gmail.com>
2013-05-10 21:35:57 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Autismhelp::Application.config.secret_token = '40a20d53a487944ea3cfb448545ce13d821d3212c275a2ff0e76bd438ae229dba89f8296ca0afbb6ee1b0d0b6a461880c995680894deef52be52b9a596b840aa'
