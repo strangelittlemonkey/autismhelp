409e37007c4e7a61bad661470c3253f8effa9737
sararob <sararob@brandeis.edu>
2013-05-11 15:21:50 -0700

    Finish pulling photos from foursquare

--- app/controllers/business_controller.rb
+++ app/controllers/business_controller.rb
@@ -10,5 +10,9 @@ class BusinessController < ApplicationController
     @lat = 40.7
     @lng = -74
     @venue_query = HTTParty.get('https://api.foursquare.com/v2/venues/search?ll=' + @lat.to_s + ',' + @lng.to_s + '&oauth_token=2YEFFRGQYVIGT2EJ30RHYLGELUCW04KHQM231ZSM3HOXQFBS&v=20130511')
+    @venue_id = JSON.parse(@venue_query.response.body)["response"]["venues"][0]["id"]
+
+    @photo_query = HTTParty.get("https://api.foursquare.com/v2/venues/" + @venue_id + "/photos?oauth_token=2YEFFRGQYVIGT2EJ30RHYLGELUCW04KHQM231ZSM3HOXQFBS&v=20130511")
+    @num_photos = JSON.parse(@photo_query.response.body)["response"]["photos"]["count"]
   end
 end
