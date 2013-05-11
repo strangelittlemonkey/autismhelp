c8fcfa607d0bd9564c78538e457aa2727719f472
Yu Jiang Tham <yoojbruin@gmail.com>
2013-05-11 14:24:02 -0700

    Added bing map search

--- app/controllers/business_controller.rb
+++ app/controllers/business_controller.rb
@@ -3,6 +3,11 @@ class BusinessController < ApplicationController
     @search_result = nil
     if params[:address].present?
       @search_result = Geocoder.coordinates(params[:address])
+      @lat = @search_result[0]
+      @lng = @search_result[1]
+    else
+      @lat = 37.795
+      @lng = -122.408
     end
   end
 
