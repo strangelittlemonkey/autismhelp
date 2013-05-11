b0a04fe25a3663ba1ab1aa2010c758573e1c2875
sararob <sararob@brandeis.edu>
2013-05-11 11:25:56 -0700

    Add bootstrap navbar to show page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,5 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-collapse.js
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Autismhelp::Application.routes.draw do
   resources :business
-
+  root to: "business#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
