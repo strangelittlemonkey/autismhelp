f7deb125833c4b82cc44add72da637623c5c3932
Yu Jiang Tham <yoojbruin@gmail.com>
2013-05-11 15:23:39 -0700

    Added search functions

--- app/controllers/business_controller.rb
+++ app/controllers/business_controller.rb
@@ -16,4 +16,7 @@ class BusinessController < ApplicationController
     @lng = -74
     @venue_query = HTTParty.get('https://api.foursquare.com/v2/venues/search?ll=' + @lat.to_s + ',' + @lng.to_s + '&oauth_token=2YEFFRGQYVIGT2EJ30RHYLGELUCW04KHQM231ZSM3HOXQFBS&v=20130511')
   end
+
+  def about
+  end
 end
