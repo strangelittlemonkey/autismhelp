1254b4143b4bff8ad11d8779695380042ff78e73
Yu Jiang Tham <yoojbruin@gmail.com>
2013-05-12 11:15:30 -0700

    Index sends id to show page

--- app/helpers/business_helper.rb
+++ app/helpers/business_helper.rb
@@ -1,2 +1,10 @@
 module BusinessHelper
+
+  def get_current_location 
+    # Current location is grabbed via html5 
+    # navigator.geolocation.getCurrentPosition when lat & lng are nil
+    @lat = nil
+    @lng = nil
+  end
+
 end
